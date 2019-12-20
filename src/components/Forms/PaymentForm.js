0973d738052584aa8180b3e8872e8963d860a724
chai28 <clomosbog@gmail.com>
2019-12-20 22:05:09 +1300

    added handleSubscribe function for submit button

--- src/components/Forms/PaymentForm.js
+++ src/components/Forms/PaymentForm.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {injectStripe, Elements, ReactStripeElements, CardElement} from 'react-stripe-elements';
+import * as React from 'react';
+import {injectStripe, Elements, CardElement} from 'react-stripe-elements';
 
 // reactstrap components
 import {
@@ -14,41 +14,67 @@ class PaymentForm extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleSubscribed = this.handleSubscribed.bind(this);
     this.state = {
-      name: ""
+      name: "",
+      amount: "",
     }
 
   document.documentElement.classList.remove("nav-open"); 
 }
-  handleSubmit = (ev) => {
+  handleSubscribed = (ev) => {
     // We don't want to let default form submission happen here, which would refresh the page.
     ev.preventDefault();
-
-    // You can also use handleCardPayment with the PaymentIntents API.
-    // See our handleCardPayment documentation for more:
-    // https://stripe.com/docs/stripe-js/reference#stripe-handle-card-payment
-    this.props.stripe.handleCardPayment('{PAYMENT_INTENT_CLIENT_SECRET}', {billing_details: {name: 'Jenny Rosen'}});
+    console.log("subscribe test");
+    try{
+      // let token = this.props.stripe.createToken({name: this.state.name});
+      // console.log(token);
+      // You can also use handleCardPayment with the PaymentIntents API.
+      // See our handleCardPayment documentation for more:
+      // https://stripe.com/docs/stripe-js/reference#stripe-handle-card-payment
+      // this.props.stripe.handleCardPayment('{PAYMENT_INTENT_CLIENT_SECRET}', {billing_details: {name: this.state.name}});
+      // console.log(this.state.name);
+    }catch(e){
+      throw e;
+    }
   };
 
   render() {
     return (
       <Elements>
-        <form className="survey-form" onSubmit={this.handleSubmit}>
+        <form className="survey-form" onSubmit={this.handleSubscribed}>
            <FormGroup>
             <label >Name</label>
             <InputGroup className="form-group-no-border">
-                <Input value={this.state.name} placeholder="Name" type="text" name="name"/>
+                <Input value={this.state.name} 
+                  placeholder="Name" 
+                  type="text" 
+                  name="name"
+                  onChange={(e) => this.setState({name: e.target.value})}
+                />
+            </InputGroup>
+          </FormGroup>
+          <FormGroup>
+            <label >Amount</label>
+            <InputGroup className="form-group-no-border">
+                <Input 
+                  value={this.state.amount} 
+                  placeholder="0.00" 
+                  type="text" 
+                  name="amount"
+                  onChange={(e) => this.setState({amount: e.target.value})}
+                />
             </InputGroup>
           </FormGroup>
           <FormGroup>
             <label>
-              Card details
+              Card details: CC Number -- Exp. Date -- CVC
             </label>
             <InputGroup className="form-group-no-border">
               <CardElement className="MyCardElement" />
             </InputGroup>
           </FormGroup>
-          <button className="btn-round btn btn-success">Subscribed</button>
+          <button type="Submit" className="btn-round btn btn-success">Subscribed</button>
         </form>
       </Elements>
     );
