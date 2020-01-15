577149184d5bb387c5fe03ad1c8aca23399538f7
chai28 <clomosbog@gmail.com>
2020-01-16 09:20:58 +1300

    added saving flow for edit profile

--- src/views/commonPage/EditProfile.jsx
+++ src/views/commonPage/EditProfile.jsx
@@ -34,13 +34,17 @@ class Policies extends React.Component {
   constructor(props) {
     super(props);
 
+    this.onChangeInput = this.onChangeInput.bind(this);
+    this.handleSaveProfile = this.handleSaveProfile.bind(this);
+
     this.state = {
       companyDetails: [],
       bNameInput: "",
       nzbnInput: "",
       bEmail: "",
-      address: "",
-      description: ""
+      bContact: "",
+      bAddr: "",
+      bDescription: ""
     };
   }
 
@@ -49,14 +53,20 @@ class Policies extends React.Component {
       user_id: localStorage.getItem("session_id")
     };
     console.log("ID: " + idInfo.user_id);
-  
+
     Axios.get("http://localhost:5000/editprofile", {
       params: { _id: localStorage.getItem("session_id") }
     })
       .then(response => {
         console.log("response", response);
         this.setState({
-          companyDetails: response.data.companyDetails
+          companyDetails: response.data.companyDetails,
+          bNameInput: response.data.companyDetails.company_name,
+          nzbnInput: response.data.companyDetails.nzbn,
+          bEmail: response.data.companyDetails.company_email,
+          bContact: response.data.companyDetails.contact,
+          bAddr: response.data.companyDetails.address,
+          bDescription: response.data.companyDetails.descption
         });
         console.log("companyDetails", this.state.companyDetails);
       })
@@ -65,21 +75,57 @@ class Policies extends React.Component {
       });
   }
 
+  onChangeInput(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  //Save button handler
+  handleSaveProfile(e) {
+    e.preventDefault();
+    console.log("handleSaveProfile clicked! ");
+    const companyDetails = {
+      _id: this.state.companyDetails._id,
+      company_name: this.state.bNameInput,
+      nzbn: this.state.nzbnInput,
+      company_email: this.state.bEmail,
+      contact: this.state.bContact,
+      address: this.state.bAddr,
+      description: this.state.bDescription
+    };
+
+    Axios.post("http://localhost:5000/editprofile", companyDetails).then(
+      res => {
+        console.log(res.data);
+      }
+    );
+  }
+
   render() {
     return (
       <>
         <div className="content">
           <Row>
             <Col className="ml-auto mr-auto">
-              <Form className="edit-profile-form">
+              <Form
+                className="edit-profile-form"
+                onSubmit={this.handleSaveProfile}
+              >
                 <FormGroup>
                   <label>
                     <h6>Business Name</h6>
                   </label>
                   <InputGroup className="form-group-no-border">
                     <Input
-                      value={this.state.companyDetails.company_name}
+                      value={this.state.bNameInput}
+                      name="bNameInput"
                       type="text"
+                      onChange={this.onChangeInput}
                     />
                   </InputGroup>
                 </FormGroup>
@@ -89,10 +135,12 @@ class Policies extends React.Component {
                   </label>
                   <Row>
                     <InputGroup className="form-group-no-border">
-                      <Col className="car-register-nzbn" lg="8">
+                      <Col lg="4">
                         <Input
-                          value={this.state.companyDetails.nzbn}
+                          value={this.state.nzbnInput}
                           type="text"
+                          name="nzbnInput"
+                          onChange={this.onChangeInput}
                         />
                       </Col>
                       <Col lg="4">
@@ -111,8 +159,10 @@ class Policies extends React.Component {
                   </label>
                   <InputGroup className="form-group-no-border">
                     <Input
-                      value={this.state.companyDetails.company_email}
+                      value={this.state.bEmail}
                       type="text"
+                      name="bEmail"
+                      onChange={this.onChangeInput}
                     />
                   </InputGroup>
                 </FormGroup>
@@ -121,10 +171,13 @@ class Policies extends React.Component {
                     <h6>Contact Number</h6>
                   </label>
                   <InputGroup className="form-group-no-border">
-                    <Input 
-                    // value={this.state.companyDetails.contact === undefined ? "add contact number" 
-                    // :this.state.companyDetails.contact} 
-                    type="number" />
+                    <Input
+                      value={this.state.bContact}
+                      placeholder="Add business contact number"
+                      type="number"
+                      name="bContact"
+                      onChange={this.onChangeInput}
+                    />
                   </InputGroup>
                 </FormGroup>
                 <FormGroup>
@@ -132,34 +185,14 @@ class Policies extends React.Component {
                     <h6>Address</h6>
                   </label>
                   <InputGroup className="form-group-no-border">
-                    <Input placeholder="Address1" type="text" />
+                    <Input
+                      placeholder="Add business address"
+                      value={this.state.bAddr}
+                      type="text"
+                      name="bAddr"
+                      onChange={this.onChangeInput}
+                    />
                   </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <label>
-                    <h6>Address 2</h6>
-                  </label>
-                  <InputGroup className="form-group-no-border">
-                    <Input placeholder="Address2" type="text" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <Row>
-                    <InputGroup className="form-group-no-border">
-                      <Col md="6">
-                        <label for="city">City</label>
-                        <Input type="text" name="city" id="city" />
-                      </Col>
-                      <Col md="4">
-                        <label for="state">State</label>
-                        <Input type="text" name="state" id="state" />
-                      </Col>
-                      <Col md="2">
-                        <label for="zip">Zip</label>
-                        <Input type="text" name="zip" id="zip" />
-                      </Col>
-                    </InputGroup>
-                  </Row>
                 </FormGroup>
                 <FormGroup>
                   <label>
@@ -169,8 +202,10 @@ class Policies extends React.Component {
                     <Input
                       type="textarea"
                       maxLength="500"
-                      name="description"
-                      id="description"
+                      name="bDescription"
+                      placeholder="Add description of business"
+                      value={this.state.bDescription}
+                      onChange={this.onChangeInput}
                       rows={4}
                       aria-multiline="true"
                     />
@@ -180,6 +215,7 @@ class Policies extends React.Component {
                   className="btn-round"
                   color="success"
                   style={{ float: "right" }}
+                  type="submit"
                 >
                   Save Changes
                 </Button>
