26f04f04eb0789efb1d290976754936cac1ab1fb
chai28 <clomosbog@gmail.com>
2020-01-28 22:33:35 +1300

    updated client reviewer view page

--- src/views/commonPage/DisplayReviewPolicy.jsx
+++ src/views/commonPage/DisplayReviewPolicy.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-// import Axios from "axios";
+import Axios from "axios";
+import { toast } from "react-toastify";
 
 // reactstrap components
 import {
@@ -9,39 +10,186 @@ import {
     InputGroup,
     Row,
     Input,
-    Container
+    Container,
+    Button
 } from "reactstrap";
 
 // core components
 import NavbarPlain from "components/Navbars/navbarPlain.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
 
-
+toast.configure();
 // import  styles from "assets/scss/paper-kit.scss";
 
 class Policies extends React.Component {
     constructor(props){
         super(props);
-
+        this.commentHandler = this.commentHandler.bind(this);
+        this.rejectHandler = this.rejectHandler.bind(this);
+        this.acceptHandler = this.acceptHandler.bind(this);
+        this.commentBtnHandler = this.commentBtnHandler.bind(this);
+        this.processBtnReq = this.processBtnReq.bind(this);
         this.state = ({
-            policyName: "",
-            companyId: "",
-            userId: "",
+            policy: [],
+            company:[],
+            commentInput: "",
+            index: ""
         });
     }
 
-    async componentDidMount(){
+    componentDidMount() {
+        let pName = this.props.match.params.policyName.replace(/-/g," ");
         if(this.props.match.params){
-            let pName = this.props.match.params.policyName.replace(/-/g," ");
-            this.setState({
-                policyName: pName,
+            // console.log(pName)
+            Axios.get("http://localhost:5000/clientReviewer", {
+                params: { _id: this.props.match.params.companyId}
+            }).then(response => {
+                this.setState({
+                    company: response.data
+                })
+                this.state.company.subscribed_policy.forEach((policy, index) => {
+                    if(policy.name === pName){
+                        this.setState({
+                            policy: policy,
+                            index: index
+                        })
+                    }
+                })
+            })
+            .catch(function(error) {
+            console.log(error);
+            });
+        
+        }
+    }
+
+    commentHandler(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+        [name]: value
+        });
+    }
+
+    rejectHandler() {
+        Axios.get("http://localhost:5000/company", {
+            params: { _id: this.props.match.params.userId, type: "user"}
+        }).then(response => {
+            const data = {
+                fname: response.data.fname,
+                lname: response.data.lname,
+                policyName: this.props.match.params.policyName.replace(/-/g," "),
                 companyId: this.props.match.params.companyId,
-                userId: this.props.match.params.userId
+                userId: this.props.match.params.userId,
+                review: "REJECTED",
+                index: this.state.index
+            }
+            this.processBtnReq(data);
+        })
+    }
+
+    processBtnReq(data) {
+        if(data.review === "ACCEPTED"){
+            Axios.post("http://localhost:5000/clientReviewer", data).then(response => {
+                // console.log(response.data);
+                console.log("status:", response.data.value);
+                if (response.data.value === "success") {
+                    toast("This policy has been accepted after review." +
+                        "Initiator will received a notification about it.", { 
+                    type: "success", 
+                    position: toast.POSITION.TOP_CENTER,
+                    onClose: ()=> {
+                        window.location.href = '/landing-page'
+                    }
+                    });
+                } else {
+                    toast("There is an error sending your review feedback", { 
+                        type: "error",
+                        position: toast.POSITION.TOP_CENTER,
+                    });
+                }
+            })
+        }else if(data.review === "REJECTED"){
+            Axios.post("http://localhost:5000/clientReviewer", data).then(response => {
+                // console.log(response.data);
+                console.log("status:", response.data.value);
+                if (response.data.value === "success") {
+                    toast("This policy has been rejected after review." +
+                        "Initiator will received a notification about it.", { 
+                    type: "success", 
+                    position: toast.POSITION.TOP_CENTER,
+                    onClose: ()=> {
+                        window.location.href = '/landing-page'
+                    }
+                    });
+                } else {
+                    toast("There is an error sending your review feedback", { 
+                        type: "error",
+                        position: toast.POSITION.TOP_CENTER,
+                    });
+                }
+            })
+        }else{
+            Axios.post("http://localhost:5000/clientReviewer", data).then(response => {
+                // console.log(response.data);
+                console.log("status:", response.data.value);
+                if (response.data.value === "success") {
+                    toast("Your review comment has been sent." + 
+                        " Initiator will received a notification about your review comment.", { 
+                    type: "success", 
+                    position: toast.POSITION.TOP_CENTER,
+                    onClose: ()=> {
+                        window.location.href = '/landing-page'
+                    }
+                    });
+                } else {
+                    toast("There is an error sending your review feedback", { 
+                        type: "error",
+                        position: toast.POSITION.TOP_CENTER,
+                    });
+                }
             })
         }
     }
 
+    acceptHandler() {
+        Axios.get("http://localhost:5000/company", {
+            params: { _id: this.props.match.params.userId, type: "user"}
+        }).then(response => {
+            const data = {
+                fname: response.data.fname,
+                lname: response.data.lname,
+                policyName: this.props.match.params.policyName.replace(/-/g," "),
+                companyId: this.props.match.params.companyId,
+                userId: this.props.match.params.userId,
+                review: "ACCEPTED",
+                index: this.state.index
+            }
+            this.processBtnReq(data);
+        })
+    }
+
+    commentBtnHandler() {
+        Axios.get("http://localhost:5000/company", {
+            params: { _id: this.props.match.params.userId, type: "user"}
+        }).then(response => {
+            const data = {
+                fname: response.data.fname,
+                lname: response.data.lname,
+                policyName: this.props.match.params.policyName.replace(/-/g," "),
+                companyId: this.props.match.params.companyId,
+                userId: this.props.match.params.userId,
+                review: "COMMENT",
+                comment: this.state.commentInput
+            }
+            this.processBtnReq(data);
+        })
+    }
+
     render(){
+        const policyContent = this.state.policy.content || []
         return (
             <>
             <NavbarPlain />
@@ -52,32 +200,59 @@ class Policies extends React.Component {
                 className="section landing-section">
             <Container>
             <Row>
-                <Col className="ml-auto mr-auto">
-                <h1 className="text-center">Policy Name</h1>
-                <p>Polic Content</p>
-                {/* <Form
-                    className="edit-profile-form"
-                    onSubmit={this.handleSaveProfile}
-                >
-                    <FormGroup>
+                <Col className="ml-auto mr-auto" md="12">
+                    <h1 className="text-center">{this.state.policy.name}</h1>
+                    {policyContent.map((content, index) => (
+                        <><p key={index}>{content}</p><br></br></>
+                    ))}
+                </Col>
+            </Row>
+            <Form>
+                <FormGroup>
                     <label>
-                        <h6>Business Name</h6>
+                        <h6>Review Comment</h6>
                     </label>
                     <InputGroup className="form-group-no-border">
                         <Input
-                        value="#############"
-                        name="bNameInput"
+                        value={this.state.commentInput}
+                        name="commentInput"
                         type="textarea"
-                        row="80"
-                        onChange={this.onChangeInput}
+                        rows="8"
+                        onChange={this.commentHandler}
                         />
-                    </InputGroup>
-                    </FormGroup>
-                    </Form> */}
-                    </Col>
-                </Row>
-                </Container>
-                </div>
+                    </InputGroup>  
+                </FormGroup>
+                <span style={{color: "red"}}>Note: </span>
+                <span style={{fontSize: "12px"}}> Click Send Comment if you have comment.
+                    Accept if Policy needs no changed and Reject if Policy can't be published.
+                </span>
+                <Button
+                  className="btn-round"
+                  color="danger"
+                  style={{ float: "right" }}
+                  onClick={this.rejectHandler}
+                >
+                  Reject
+                </Button>
+                <Button
+                  className="btn-round"
+                  color="success"
+                  style={{ float: "right" }}
+                  onClick={this.acceptHandler}
+                >
+                  Accept
+                </Button>
+                <Button
+                  className="btn-round"
+                  color="info"
+                  style={{ float: "right" }}
+                  onClick={this.commentBtnHandler}
+                >
+                  Send Comment
+                </Button>
+            </Form>
+            </Container>
+            </div>
             <DemoFooter />
             </>
         );
